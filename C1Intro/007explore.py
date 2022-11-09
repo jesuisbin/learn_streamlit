b88a107b2276526a2939c1846f1f396777aff674
bin <ntupham@groupeseb.com>
2022-11-09 15:28:45 +0700

    Add chap 1

--- C1Intro/007explore.py
+++ C1Intro/007explore.py
@@ -6,6 +6,7 @@ import streamlit as st
 # Func
 def main():
     """All your code goes here"""
+    st.title("Hello Streamlit")
     pass
 
 if __name__ == '__main__':
---
+++ C1Intro/008displaytxt.py
@@ -0,0 +1,21 @@
+# Basics & Fundamentals
+
+# Core Packages
+import streamlit as st
+
+# Working and Displaying Text
+st.text("Hello World this is a text")
+name = "Binpe"
+st.text("This is {}".format(name))
+
+# Title
+st.title("This is a title")
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Markdown
+st.markdown("This is a markdown")
